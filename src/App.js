e73cf57c0d93f6cb561049e495e637938860f819
santi vittor <santi.vittor-@live.com.ar>
2021-08-16 15:59:58 -0300

    Entrega de clase 4: Componentes 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import NavBar from "./components/nav/NavBar"
+import NavBar from "./components/nav/NavBar";
+import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./estilos.css"
+
 const App = () => {
     return (
         <>
         <NavBar/>
+        <ItemListContainer nombre="Santi"/>
         </>
     )
 }
---
+++ src/components/itemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+const ItemListContainer = (props) => {
+    return ( 
+        <>
+        <div className="saludoBienvenida">
+        <h2>Hola! Bienvenido, {props.nombre}</h2>
+        </div>
+        </>
+     );
+}
+ 
+export default ItemListContainer;
